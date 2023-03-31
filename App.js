7ac042b2bd43c970d4fac203767e0d1d4835b719
ShorenaK <shorenaanzhilov@gmail.com>
2023-03-31 17:59:21 -0400

    style.css set on Hero sectoin

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 
-
 export default function App(){
     return (
         <div> 
--- components/Hero.js
+++ components/Hero.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 export default function img() {
   return (
-    <div>
-        <img src="..images/photo-grid.png" alt="rendom pictures" className=''/> 
-        <h1>Online Expreiences</h1>
+    <section className='hero'>
+        <img src="..images/photo-grid.png" alt="rendom pictures" className='hero__photo'/> 
+        <h1 className='hero__header'>Online Expreiences</h1>
 
-    <h4>Join unique interactive activites led by 
-        one-of-akind host-all without leaving home.</h4>
-    </div>
+    <p className='hero__text'>Join unique interactive activites led by 
+        one-of-akind host-all without leaving home.</p>
+    </section>
   )
 }
