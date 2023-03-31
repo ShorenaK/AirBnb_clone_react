17f748d47b3fcaf0beacbdbdd44769289836f5fc
ShorenaK <shorenaanzhilov@gmail.com>
2023-03-31 17:51:45 -0400

    Hero component added

---
+++ components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function img() {
+  return (
+    <div>
+        <img src="..images/photo-grid.png" alt="rendom pictures" className=''/> 
+        <h1>Online Expreiences</h1>
+
+    <h4>Join unique interactive activites led by 
+        one-of-akind host-all without leaving home.</h4>
+    </div>
+  )
+}
