182eb64c92895c8282590948eeccf11d848bcee9
ShorenaK <shorenaanzhilov@gmail.com>
2023-04-01 16:50:19 -0400

    card.js updated

--- components/Card.js
+++ components/Card.js
@@ -2,12 +2,16 @@ import React from 'react'
 
 export default function Card() {
   return (
-    <div>
-        <h1> <img src="../images/katie-zaferes.png" alt="photo" /></h1>
-        
-        <p> <img src="../images/star.png" alt="star icon picture" /> 5.0 (30) USA   </p>
+    <div className='card'>
+         <img src="../images/katie-zaferes.png" alt="photo"  className='card__image'/>
+        <div className='card__stats'> 
+         <img src="../images/star.png" alt="star icon picture" className="card__star"/> 
+         <span> 5.0</span>   
+         <span> (30)</span>  
+         <span>  USA </span> 
+         </div>
+         <p> Life lesson with Katie Zaferes</p>
         <p> From $125 / person </p>
-
     </div>
   )
 }
