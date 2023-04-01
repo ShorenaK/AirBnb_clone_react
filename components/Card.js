049a679793ba47329bfda62b36cdeefa5c0a57e3
ShorenaK <shorenaanzhilov@gmail.com>
2023-04-01 16:37:29 -0400

    Card.jshas been added

---
+++ components/Card.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Card() {
+  return (
+    <div>
+        <h1> <img src="" alt="" /></h1>
+        <p></p>
+        <p></p>
+
+    </div>
+  )
+}
