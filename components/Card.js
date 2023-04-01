4b5311d6add468553dc96a70aeaa31ad3a96a747
ShorenaK <shorenaanzhilov@gmail.com>
2023-04-01 16:40:27 -0400

    images added

--- components/Card.js
+++ components/Card.js
@@ -3,8 +3,9 @@ import React from 'react'
 export default function Card() {
   return (
     <div>
-        <h1> <img src="" alt="" /></h1>
-        <p></p>
+        <h1> <img src="../images/katie-zaferes.png" alt="photo" /></h1>
+        <img src="../images/star.png" alt="" />
+        <p> </p>
         <p></p>
 
     </div>
