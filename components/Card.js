d2ba08c235b026eb93c5ddb3442aabed9283017b
ShorenaK <shorenaanzhilov@gmail.com>
2023-04-04 19:48:28 -0400

    card.js updated

--- components/Card.js
+++ components/Card.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Card(props) {
   return (
     <div className='card'>
-         <img src={props.img} alt="photo"  className='card__image'/>
+         <img src={`../images/{props.img}`} alt="photo"  className='card__image'/>
         <div className='card__stats'> 
          <img src="../images/star.png" alt="star icon picture" className="card__star"/> 
          <span className='span'> {props.rating}</span>   
-         <span className='span'> {props.reviewCount}</span>  
+         <span className='span'> ({props.reviewCount})</span>  
          <span className='span'> {props.country}</span> 
          </div>
          <p> {props.title}</p>
