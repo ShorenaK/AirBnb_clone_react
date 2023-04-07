c0119095e92e02516cf6a4ac672f23943642f174
ShorenaK <shorenaanzhilov@gmail.com>
2023-04-07 15:10:22 -0400

    app.js refactored

--- App.js
+++ App.js
@@ -2,29 +2,26 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
-import data from "./data.js"
-console.log(data)
-
+import data from "data.js"
 
 export default function App(){
-    //hero
-    data.map((com)=>{
+    const cards = data.map((item)=>{
+        < Card 
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+        /> 
+    })
     return (
         <div> 
             <Navbar />
             <Hero /> 
-            < Card 
-            id = {com.id}
-            img= "katie-zaferes.png"
-            rating={com.rating}
-            reviewCount={com.reviewCount}
-            location={com.location}
-             title={com.title}
-             price={136}    
-            
-            /> 
+           {cards}
         </div>
 
     )
-})
+
 }
