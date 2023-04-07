4e763fc774e732e2c3af72f930466ad5e2238fda
ShorenaK <shorenaanzhilov@gmail.com>
2023-04-07 16:34:34 -0400

    Card.js logic has been impelmented

--- components/Card.js
+++ components/Card.js
@@ -1,9 +1,18 @@
 import React from 'react'
 
 export default function Card(props) {
+    let badgeText
+  if(props.openSpot === 0 ){
+    badgeText = "SOLD OUT"
+  }else if(props.location === "Online"){
+      badgeText = "ONLINE"
+  }
+
+
+
   return (
     <div className='card'>
-       {props.openSpot === 0 && <div className="card--badge">SOLD OUT</div>}
+       {badgeText && <div className="card--badge">{badgeText}</div>}
          <img src={`../images/{props.img}`} alt="photo"  className='card__image'/>
         <div className='card__stats'> 
          <img src="../images/star.png" alt="star icon picture" className="card__star"/> 
